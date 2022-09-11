9f569e3b5771e2d9b172f87818d7869d3b631b32
rakeshranjan04 <rakeshranjan.india@gmail.com>
2022-09-11 14:10:24 -0600

    Initial version - Clean Slate

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+      <title>Twitter Clone</title>
+      </Head>
+     <h1>Twitter Clone</h1>
+  
+
+    </div>
+  )
+}
